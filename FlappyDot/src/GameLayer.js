f3be415ccf0c740b3c10e50b871400b62971ca12
Not <patinya.y@ku.th>
2015-02-25 17:59:23 +0700

    No error for dot.png

--- FlappyDot/src/GameLayer.js
+++ FlappyDot/src/GameLayer.js
@@ -1,4 +1,17 @@
-this.player = new Player();
+var GameLayer = cc.LayerColor.extend({
+	init: function() {
+		this.player = new Player();
         this.player.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2 ) );
         this.addChild( this.player );
         this.player.scheduleUpdate();
+	}
+});
+
+var StartScene = cc.Scene.extend({
+    onEnter: function() {
+        this._super();
+        var layer = new GameLayer();
+        layer.init();
+        this.addChild( layer );
+    }
+});
