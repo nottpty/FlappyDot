60f511b77e655fb0415b7d729a98d147bb4f85d9
Not <patinya.y@ku.th>
2015-02-25 18:34:59 +0700

    Bird can jump

--- FlappyDot/src/GameLayer.js
+++ FlappyDot/src/GameLayer.js
@@ -3,6 +3,7 @@ var GameLayer = cc.LayerColor.extend({
 		this.player = new Player();
         this.player.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2 ) );
         this.addChild( this.player );
+        this.player.addKeyboardHandlers();
         this.player.scheduleUpdate();
 	}
 });
--- FlappyDot/src/Player.js
+++ FlappyDot/src/Player.js
@@ -9,8 +9,33 @@ var Player = cc.Sprite.extend({
         var pos = this.getPosition();
         this.setPosition( new cc.Point( pos.x, pos.y + this.vy ) );
         this.vy += -1;
-    }
+    },
+
+    jump: function() {
+        this.vy = Player.JUMPING_VELOCITY;
+    },
+ 
+    onKeyDown: function( keyCode, event ) {
+    	this.jump();
+    },
+ 
+    onKeyUp: function( keyCode, event ) {
+    },
+    addKeyboardHandlers: function() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed : function( keyCode, event ) {
+                self.onKeyDown( keyCode, event );
+            },
+            onKeyReleased: function( keyCode, event ) {
+                self.onKeyUp( keyCode, event );
+            }
+        }, this);
+    },
+    
 });
 
 Player.G = -1;
 Player.STARTING_VELOCITY = 15;
+Player.JUMPING_VELOCITY = 15;
