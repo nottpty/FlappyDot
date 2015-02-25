36346454c22466d8202b5c5701d533068f6da3a5
Not <patinya.y@ku.th>
2015-02-25 18:16:59 +0700

    Bird can falling down

--- FlappyDot/src/Player.js
+++ FlappyDot/src/Player.js
@@ -2,5 +2,15 @@ var Player = cc.Sprite.extend({
     ctor: function() {
         this._super();
         this.initWithFile( 'res/images/dot.png' );
+        this.vy = 15;
+    },
+
+    update: function( dt ) {
+        var pos = this.getPosition();
+        this.setPosition( new cc.Point( pos.x, pos.y + this.vy ) );
+        this.vy += -1;
     }
 });
+
+Player.G = -1;
+Player.STARTING_VELOCITY = 15;
